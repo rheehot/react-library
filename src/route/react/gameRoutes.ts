ae72fc4f5bce67dc8ebab8b5de4e095e3721af12
choiyujin <dbwls6840@naver.com>
2020-02-19 06:36:09 +0900

    헤더 메뉴에서 구구단, 끝말잇기 삭제.

--- src/route/react/gameRoutes.ts
+++ src/route/react/gameRoutes.ts
@@ -1,27 +1,15 @@
-import MultiplicationTableQuiz from "../../study/react/game/multiplication-table/MultiplicationTableQuiz";
-import EndingWordGame from "../../study/react/game/ending-word/EndingWordGame";
 import MineSweeper from "../../study/react/game/mine-sweeper/MineSweeper";
 import TicTacToeHome from "../../study/react/game/tic-tac-toe/TicTacToeHome";
 
 export const gameRoutes = [
     {
-        path: "/react/game/multiplication-table",
-        component: MultiplicationTableQuiz,
-        exact: true
-    },
-    {
-        path: "/react/game/ending-game",
-        component: EndingWordGame,
+        path: "/react/game/tic-tac-toe",
+        component: TicTacToeHome,
         exact: true
     },
     {
         path: "/react/game/mine-sweeper",
         component: MineSweeper,
         exact: true
-    },
-    {
-        path: "/react/game/tic-tac-toe",
-        component: TicTacToeHome,
-        exact: true
-    },
+    }
 ];
