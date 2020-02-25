890b0c0375d18353043087c123f725c466386ebf
choiyujin <dbwls6840@naver.com>
2020-02-25 22:16:49 +0900

    React.memo 동작 확인했음. 공식문서대로, props 변화만 체크해줌. 그리고 함수컴포넌트 기본은, props 안바꾸고 부모컴포넌트의 render()가 호출되면 기냥 자식도 render()호출되버림.

--- src/route/react/renderRoutes.ts
+++ src/route/react/renderRoutes.ts
@@ -1,18 +1,29 @@
 import PureComponentParent from "../../study/react/render/pure/PureComponentParent";
 import PropReRenderParent from "../../study/react/render/prop-re-render/PropReRenderParent";
+import ClassComponentParent from "../../study/react/render/function/ClassComponentParent";
+
+const path = "/react/render";
 
 export const renderRoutes = [
     {
-        path: "/react/render/pure",
+        path: `${path}/pure`,
         component: PureComponentParent,
         exact: true
     },
     {
-        path: "/react/render/props-re-render-test",
+        path: `${path}/props-re-render-test`,
         component: PropReRenderParent,
         exact: true,
         meta: {
             name: "Props Re-render"
+        }
+    },
+    {
+        path: `${path}/functional`,
+        component: ClassComponentParent,
+        exact: true,
+        meta: {
+            name: "함수형 컴포넌트"
         }
     }
 ];
