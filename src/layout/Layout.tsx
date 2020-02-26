88941bef0d094e6ffd9e572cc465e877bf941010
choiyujin <dbwls6840@naver.com>
2020-02-26 09:22:35 +0900

    Layout hooks로 변경 완료

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,45 +6,30 @@ import {reactHeader} from "./header/ReactHeader";
 import {cssHeader} from "./header/CssHeader";
 import HeaderInfo from "./header/HeaderInfo";
 
-interface AppProp {
-}
 
-interface AppState {
-    headerUls: Array<Array<HeaderInfo>>
-}
+export const Layout = () => {
 
-export default class Layout extends React.Component<AppProp, AppState> {
+    const [headerUls, setHeaderUls] = React.useState(reactHeader);
 
-    constructor(props: AppProp) {
-        super(props);
+    const headerChange = (headerInfo: HeaderInfo) => {
 
-        this.state = {
-            headerUls: reactHeader
-        };
-
-        this.headerChange = this.headerChange.bind(this);
-    }
-
-    headerChange(headerInfo: HeaderInfo) {
         const firstDir = headerInfo.to.split("/")[1];
 
         switch (firstDir) {
             case "css":
-                this.setState({headerUls: cssHeader});
+                setHeaderUls(cssHeader);
                 break;
 
             default:
-                this.setState({headerUls: reactHeader});
+                setHeaderUls(reactHeader);
         }
-    }
+    };
 
-    render() {
-        return (
-            <>
-                <MyHeader headerUls={this.state.headerUls} headerChange={this.headerChange}></MyHeader>
-                <MySection></MySection>
-                <MyFooter></MyFooter>
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <MyHeader headerUls={headerUls} headerChange={headerChange}></MyHeader>
+            <MySection></MySection>
+            <MyFooter></MyFooter>
+        </>
+    );
+};
