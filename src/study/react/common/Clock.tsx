747b01789bd25cd9446a60b5dae54d9e825ce389
choiyujin <dbwls6840@naver.com>
2020-02-26 10:26:45 +0900

    useEffect를 활용한 Clock Component 1번 제안

--- src/study/react/common/Clock.tsx
+++ src/study/react/common/Clock.tsx
@@ -7,11 +7,22 @@ export const Clock = () => {
 
     const [dateFormat, setDateFormat] = React.useState(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-    const startClock = () => {
-        setInterval(() => {
+    React.useEffect(() => {
+
+        console.log("effect call");
+
+        const intervalId = setInterval(() => {
             setDateFormat(moment().format('MMMM Do YYYY, h:mm:ss a'))
         }, 1000);
-    };
+
+        return () => {
+
+            console.log("effect end");
+
+            clearInterval(intervalId);
+        };
+
+    }, []);
 
     return (
         <div className="clock-wrap">
