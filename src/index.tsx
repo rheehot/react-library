9baaf7e4164b3fc5cd8a5a9038442f9f544f4171
choiyujin <dbwls6840@naver.com>
2020-02-27 14:21:55 +0900

    redux devtools 붙여봤음.

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,10 @@ import Layout from "./layout/Layout";
 import {createStore} from "redux";
 import rootReducer from "./store/modules";
 
-const store = createStore(rootReducer);
+
+//@ts-ignore
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
 console.log(store.getState());
 
 ReactDOM.render(
