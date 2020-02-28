5f69eea47412cabeba56825bd96024adfc219efd
choiyujin <dbwls6840@naver.com>
2020-02-28 19:17:49 +0900

    필요없는 파일 삭제

--- src/route/react/exampleRoutes.ts
+++ src/route/react/exampleRoutes.ts
@@ -1,6 +1,5 @@
 import Counter from "../../study/react/example/counter/Counter";
 import RenderProp from "../../study/react/example/render-prop/RenderProp";
-import MultiplicationTableQuiz from "../../study/react/example/multiplication-table/MultiplicationTableQuiz";
 import TicTacToeHome from "../../study/react/example/tic-tac-toe/TicTacToeHome";
 
 const path = `/react/example`;
@@ -19,11 +18,6 @@ export const exampleRoutes = [
     {
         path: `${path}/render-prop`,
         component: RenderProp,
-        exact: true
-    },
-    {
-        path: `${path}/multiplication-table`,
-        component: MultiplicationTableQuiz,
         exact: true
     },
 ];
