3adc863f9056fe086eff1140091deb152714e022
choiyujin <dbwls6840@naver.com>
2020-03-11 02:59:34 +0900

    새로고침했을 때 url이 /css/main이면 css의 메인화면이 나와야하는데, header의 기본값이 react로 맞춰서있어서 새로고침하면 react로 나오는 문제가 있었음. 그 이유는 useState(reactHeader)로 되어있었고.
    그래서,
    (1) react / css main이 바뀌면 header가 바뀌면서 다시 렌더링되야하므로 header는 state로 만들어야하고,
    (2) 최초 렌더링 이전에 url을 계산하여 그에맞는 초기값을 제공해야하는 2가지 조건이 있었음.
    
    그래서 hooks공식문서를 살펴본 결과
    useState()에 콜백함수를 전달해주면 된다고했는데
    
    문제는 "초기 state가 고비용 계산의 결과라면, 초기 렌더링 시에만 실행될 함수를 대신 제공할 수 있습니다."
    
    이렇게 안내하고있고, 저거는 고비용이 아님...
    그래서 이거보다 더 나은방법이 있는지 아직 모르겠음.

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,28 @@
 import * as React from "react";
+import {useState} from "react";
 import MyHeader from "./MyHeader";
 import MySection from "./MySection";
 import MyFooter from "./MyFooter";
 import {reactHeader} from "./header/ReactHeader";
 import {cssHeader} from "./header/CssHeader";
 import HeaderInfo from "./header/HeaderInfo";
-import {useState} from "react";
 
 export default function Layout() {
 
-    const [headerUls, setHeaderUls] = useState(reactHeader);
+    const [headerUls, setHeaderUls] = useState(() => {
+
+        console.log("setState callback call");
+
+        const firstDir = location.pathname.split("/")[1];
+
+        switch (firstDir) {
+            case "css":
+                return cssHeader;
+
+            default:
+                return reactHeader;
+        }
+    });
 
     const headerChange = (headerInfo: HeaderInfo) => {
 
@@ -25,11 +38,13 @@ export default function Layout() {
         }
     };
 
+    console.log("return");
+
     return (
         <>
-            <MyHeader headerUls={headerUls} headerChange={headerChange}></MyHeader>
-            <MySection></MySection>
-            <MyFooter></MyFooter>
+            <MyHeader headerUls={headerUls} headerChange={headerChange}/>
+            <MySection/>
+            <MyFooter/>
         </>
     );
 };
