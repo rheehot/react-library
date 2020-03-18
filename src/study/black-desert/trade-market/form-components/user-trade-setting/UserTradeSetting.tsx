66afd8a963c0a93ca57f6303319b781987a80101
choiyujin <dbwls6840@naver.com>
2020-03-18 19:33:09 +0900

    useRef에 초기값을 지정할 때, 예시처럼 계산이 필요한 경우 난감했다.
    일단 공식문서에서는 초기값으로 함수를 전달하는 케이스는 없었음. 그래서 ref.current()라는 사용예시도 없었고.
    
    우선 처음 만들어본 에제는 함수를 초기값으로 지정하고 ref.current()라고 사용하는것.

--- src/study/black-desert/trade-market/form-components/user-trade-setting/UserTradeSetting.tsx
+++ src/study/black-desert/trade-market/form-components/user-trade-setting/UserTradeSetting.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import {ChangeEvent, useEffect, useRef} from "react"
+import {ChangeEvent, useRef} from "react"
 import RadioGroup from "../../../../react/common/form/RadioGroup";
 import {getHeraldryFameStepAmount, HERALDRY_FAME_ARRAY} from "../../../system-setting/TradeSystem";
 import UserInfo from "../../../common/UserInfo";
@@ -8,21 +8,18 @@ import "../TradeMarketForm.scss";
 
 export default function UserTradeSetting(props: UserTradeSettingProp) {
 
-    const heraldryFameArray = useRef([]);
-
-    useEffect(() => {
+    const heraldryFameArray = useRef(() => {
 
         const withoutLast = HERALDRY_FAME_ARRAY.slice(0, HERALDRY_FAME_ARRAY.length - 1);
         const last = HERALDRY_FAME_ARRAY[HERALDRY_FAME_ARRAY.length - 1];
 
-        heraldryFameArray.current = withoutLast.map((heraldryFame, index) => {
+        return withoutLast.map((heraldryFame, index) => {
             return {
                 value: heraldryFame.amount,
                 label: `${heraldryFame.amount}점 이상 ${HERALDRY_FAME_ARRAY[index + 1].amount}점 미만`
             }
         }).concat({value: last.amount, label: `${last.amount}점 이상`});
-
-    }, []);
+    });
 
     function setStateHaveValuePackage(event: ChangeEvent<HTMLInputElement>) {
         props.changeUserInfo(Object.assign<{}, UserInfo, {haveValuePackage: boolean}>({}, props.userInfo, {haveValuePackage: event.target.checked}));
@@ -37,7 +34,7 @@ export default function UserTradeSetting(props: UserTradeSettingProp) {
             <div className="form-header">
                 <fieldset>가문명성 선택</fieldset>
             </div>
-            <RadioGroup selectValue={getHeraldryFameStepAmount(props.userInfo.heraldryFame)} valueAndLabelArray={heraldryFameArray.current} radioGroupName="HeraldryFame" selectHandler={setStateHeraldryFame}/>
+            <RadioGroup selectValue={getHeraldryFameStepAmount(props.userInfo.heraldryFame)} valueAndLabelArray={heraldryFameArray.current()} radioGroupName="HeraldryFame" selectHandler={setStateHeraldryFame}/>
 
             <input type="checkbox" onChange={setStateHaveValuePackage} checked={props.userInfo.haveValuePackage}/>
             <label>밸류패키지 여부</label>
